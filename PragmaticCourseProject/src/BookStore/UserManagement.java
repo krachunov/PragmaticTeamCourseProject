87f7515425c36ada0ae34e5250b9d210bbdae446
Krachunov <Krachunov@Krachunov-PC>
2015-11-19 10:54:23 +0200

    Change UserManagment class create user method

--- PragmaticCourseProject/src/BookStore/UserManagement.java
+++ PragmaticCourseProject/src/BookStore/UserManagement.java
@@ -1,23 +1,24 @@
 package BookStore;
 
-public class UserManagement implements IUserManagement{
+public class UserManagement implements IUserManagement {
 	private int currentNumOfUsers = 0;
 	private User[] users = new User[10];
-	
-	public User selectUserById(int userId){
+
+	public User selectUserById(int userId) {
 		return users[userId];
 	}
-	
-	public int createUser(String username, String password){
-		this.users[currentNumOfUsers] = new User();
-		this.users[currentNumOfUsers].setUsername(username);
-		this.users[currentNumOfUsers].setPassword(password);
-		this.users[currentNumOfUsers].permissions = new Permissions();
+
+	public int createUser(String username, String password) {
+		User newUser = new User();
+		newUser.setUsername(username);
+		newUser.setPassword(password);
+		newUser.permissions = new Permissions();
+		this.users[currentNumOfUsers] = newUser;
 		return currentNumOfUsers++;
 	}
-		
+
 	public User logIn(String username, String password) {
-		for(int i = 0; i< currentNumOfUsers; i++) {
+		for (int i = 0; i < currentNumOfUsers; i++) {
 			if (username.equals(this.users[i].getUsername())) {
 				if (this.users[i].getPassword().equals(password))
 					return this.users[i];
@@ -28,5 +29,3 @@ public class UserManagement implements IUserManagement{
 		return null;
 	}
 }
-
-
